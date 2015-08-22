30b69c71736d80143bb7fa0773f06582be845655
Szőke Szabolcs <szabolcsx95@gmail.com>
2015-08-22 13:48:27 +0300

    Események feldolgozása

---
+++ Firmware/Source/event.c
@@ -0,0 +1,15 @@
+#include <event.h>
+
+#include <lcd2.h>
+#include <io_mapping.h>
+#include <flags.h>
+
+inline void handleEvents(void)     /* Eseményel feldolgozása */
+{
+    if(eventFlags.updateLcdRequested)
+    {
+        eventFlags.updateLcdRequested = 0;
+
+        lcdwStringAt(&lcdHandle, 1, 1, "Hello world!");
+    }
+}
