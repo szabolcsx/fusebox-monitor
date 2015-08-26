a29619a9135d9d81fcccbad233a5e945ec7fee5b
Szőke Szabolcs <szabolcsx95@gmail.com>
2015-08-26 20:15:14 +0300

    Kommentek hozzáadva

--- Firmware/Include/sfr_helper.h
+++ Firmware/Include/sfr_helper.h
@@ -1,11 +1,11 @@
 #ifndef __SFR_HELPER__
 #define __SFR_HELPER__
 
-#define _V(SFR, N)     ((SFR >> N) & 1)                // Bit value
-#define _SB(SFR, N)     SFR |= (1 << N)                 // Set N-th bit
-#define _CB(SFR, N)     SFR &=~(1 << N)                 // Celear N-th bit
-#define _NB(SFR, N, X)  SFR ^= (-X ^ SFR) & (1 << N)    // Value of N-t bit = X X->[0,1]
-#define _INPUT(SFR, N)  _CB(DDR##SFR, N)
-#define _OUTPUT(SFR, N)  _SB(DDR##SFR, N)
+#define _V(SFR, N)      ((SFR >> N) & 1)                  // Bit value
+#define _SB(SFR, N)     (SFR |= (1 << N))                 // Set N-th bit
+#define _CB(SFR, N)     (SFR &=~(1 << N))                 // Celear N-th bit
+#define _NB(SFR, N, X)  (SFR ^= (-X ^ SFR) & (1 << N))    // Value of N-t bit = X X->[0,1]
+#define _INPUT(SFR, N)  _CB(DDR##SFR, N)                  // Set the N-th pin on port input: _INPUT(D, 0)
+#define _OUTPUT(SFR, N) _SB(DDR##SFR, N)                  // Set the N-th pin on port output: _OUTPUT(D, 0) 
 
 #endif /* __SFR_HELPER__ */
